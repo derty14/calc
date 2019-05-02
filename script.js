3cd88129b7644518d15c5b6d6c5db46c89d19c9e
derty14 <2506505q@mail.ru>
2019-05-02 14:01:31 +0300

    sus

--- script.js
+++ script.js
@@ -1,36 +1,17 @@
+sub.onclick=function () {
+  var material=document.getElementById("material").value,//price of materials
+  answer=document.getElementById("answer"),
+  number=document.getElementById("size").value;//number of materials
+  console.log(number);
+  if (!number){
+    answer.style.display="none"
+    error.style.display="block";
+  }
+  else {
+    error.style.display="none"
+    console.log(material);
+    answer.style.display="block";
+    answer.innerHTML = "Total: " + number*material+" Rub ";
 
-
-$(document).ready(function(){
- $("#sub").bind("click" , dima)
-function dima() {
-
-
-var a,b,c;
-a=$("#material").val();
-switch (a) {
-  case 'linol':
-    a=165;
-    break;
-  case 'kovr' :
-    a=178;
-    break;
-  default:
-    a=1200;
-    break;
-};
-b=$("#size").val();
-
-
-if (!$('#size').val()) {
-  $('#error').show();
-  $('#answer').hide();
-  $('#size').css("border-color","#f44e4e");
-}
-else {
-  $('#size').css("border-color","white");
-  $('#answer').show();
-  $('#error').hide();
-  $("#answer").html("<div class='answer'><p> Итого: "+(a*b)+" руб</p><div>")
   }
 }
-});
