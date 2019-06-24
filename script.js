2918907de1be1c877a566d871401ee7b4f2a4044
derty14 <2506505q@mail.ru>
2019-06-24 14:41:37 +0300

    update

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-sub.onclick=function () {
-  var material=document.getElementById("material").value,//price of materials
-  answer=document.getElementById("answer"),
-  number=document.getElementById("size").value;//number of materials
-  console.log(number);
-  if (!number){
-    answer.style.display="none"
-    error.style.display="block";
-  }
-  else {
-    error.style.display="none"
-    console.log(material);
-    answer.style.display="block";
-    answer.innerHTML = "Total: " + number*material+" Rub ";
-
-  }
+const material=document.getElementById("material"),//price of materials
+answer=document.getElementById("answer"),
+number=document.getElementById("size");//number of materials
+number.onkeypress=event=>{
+  let number=Number(event.key);
+  if (isNaN(number)){
+  return false ;
+}
+else {
+   return true ;
+}
+}
+sub.onclick= ()=>{
+  (!number.value)? (
+     answer.style.display="none",
+    error.style.display="block"
+  )
+  :(
+    error.style.display="none",
+    answer.style.display="block",
+    answer.innerHTML = "Total: " + number.value*material.value+" Rub "
+  );
 }
